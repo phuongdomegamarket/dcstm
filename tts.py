4fd617524104a9e79f6ccc6aad53ac0c4ca356c6
alptangal <alptangal@gmail.com>
2026-02-10 08:34:36 +0700

    .

--- tts.py
+++ tts.py
@@ -21,7 +21,7 @@ def process(content: str, extra_tts_keys=None):
             payload = content
             headers = {
                 "api-key": ttsKey,
-                "speed": "-0.5",
+                "speed": "",
                 "voice": "banmai",
             }
 
